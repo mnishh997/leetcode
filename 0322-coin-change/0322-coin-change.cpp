085804ae12daca3300bc43c10f460c391677ed8e
mnishh997 <mnishh997@gmail.com>
2024-06-19 22:07:32 +0530

    Time: 37 ms (50.45%), Space: 46.8 MB (35.26%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,38 +1,30 @@
 class Solution {
 public:
-    int coinChange(vector<int>& arr, int T) {
-        int n = arr.size();
-        vector<vector<int>> dp(n, vector<int>(T + 1, 0));
-        for (int i = 0; i <= T; i++) {
-        if (i % arr[0] == 0)
-            dp[0][i] = i / arr[0];
-        else
-            dp[0][i] = 1e9; // Set it to a very large value if not possible
-    }
-    
-    // Fill the DP table using a bottom-up approach
-    for (int ind = 1; ind < n; ind++) {
-        for (int target = 0; target <= T; target++) {
-            // Calculate the minimum elements needed without taking the current element
-            int notTake = dp[ind - 1][target];
-            
-            // Calculate the minimum elements needed by taking the current element
-            int take = 1e9; // Initialize 'take' to a very large value
-            if (arr[ind] <= target)
-                take = 1 + dp[ind][target - arr[ind]];
-                
-            // Store the minimum of 'notTake' and 'take' in the DP table
-            dp[ind][target] = min(notTake, take);
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount+1, 0));
+
+        for(int i=0;i<n;i++){
+            dp[i][0]=0;
         }
-    }
-    
-    // The answer is in the bottom-right cell of the DP table
-    int ans = dp[n - 1][T];
-    
-    // If 'ans' is still very large, it means it's not possible to form the target sum
-    if (ans >= 1e9)
-        return -1;
-    
-    return ans;
+        for(int i=0;i<=amount;i++){
+            if(i%coins[0]==0){
+                dp[0][i]=i/coins[0];
+            }
+            else{
+                dp[0][i]=1e9;
+            }
+        }
+
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=amount;j++){
+                dp[i][j]=dp[i-1][j];
+                if(j>=coins[i])dp[i][j]=min(dp[i][j],1+dp[i][j-coins[i]]);
+            }
+        }
+
+        if(dp[n-1][amount]==1e9)return -1;
+        return dp[n-1][amount];
+        
     }
 };
