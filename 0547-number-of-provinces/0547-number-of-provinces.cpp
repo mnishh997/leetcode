c3cae9e6c83a867742328e9883180e42c6fbc856
mnishh997 <mnishh997@gmail.com>
2024-07-08 19:01:56 +0530

    Time: 17 ms (84.65%), Space: 17.9 MB (88.1%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,42 +1,92 @@
-class Solution {
-    void bfs(vector<int>& vis, vector<vector<int>>& adj, int node){
-        vis[node]=1;
-        queue<int> q;
-        q.push(node);
-        while(!q.empty()){
-            int n = q.front();
-            q.pop();
-            for(auto it: adj[n]){
-                if(vis[it]==0){
-                    q.push(it);
-                    vis[it]=1;
-                }
+#include<bits/stdc++.h>
+#define M 1000000007
+#define ll long long 
+#define lld long double
+#define frr(i,a,b)  for(ll i=a;i>=b;i--)
+#define pb push_back
+#define popb pop_back
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+#define f first
+#define sec second
+#define fr(i,a,b) for(ll i=a;i<b;i++) 
+#define rep(i,a,b) for(ll i=a;i<=b;i++)
+#define sz(a) (int)a.size()
+#define ump unordered_map
+#define vec(v,n) vector<ll> v(n)
+#define cin(v) for(ll&it:v)cin>>it
+
+using namespace std;
+
+class DisjointSet{
+    
+    public:
+    vector<ll> rank, size, parent;
+        DisjointSet(ll n){
+            rank.resize(n+1, 0);
+            size.resize(n+1, 1);
+            parent.resize(n+1);
+            fr(i,0,n+1){
+                parent[i] = i;
             }
         }
-    }
+        
+        ll find_par(ll u){
+            if(parent[u] == u)return u;
+            
+            return parent[u] = find_par(parent[u]);
+        }
+        void union_by_rank(ll u, ll v){
+            ll ulp_u = find_par(u);
+            ll ulp_v = find_par(v);
+            if(ulp_u == ulp_v)return;
+            if(rank[ulp_u] > rank[ulp_v]){
+                parent[ulp_v] = ulp_u;
+            }
+            else if(rank[ulp_u] < rank[ulp_v]){
+                parent[ulp_u] = ulp_v;
+            }
+            else{
+                parent[ulp_u] = ulp_v;
+                rank[ulp_u]++;
+            }
+        }
+        void union_by_size(ll u, ll v){
+            ll ulp_u = find_par(u);
+            ll ulp_v = find_par(v);
+            if(ulp_u == ulp_v)return;
+            if(size[ulp_u] < size[ulp_v]){
+                parent[ulp_u] = ulp_v;
+                size[ulp_v]+=size[ulp_u];
+            }
+            else{
+                parent[ulp_v] = ulp_u;
+                size[ulp_u]+=size[ulp_v];
+            }
+        }
+};
+
+class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int res = 0;
+        int cnt = 0;
         int n = isConnected.size();
-        // first of all i need an adjacency list
-        vector<vector<int>> adj(n,vector<int>());
-        for(int i=0;i<n;i++){
+        DisjointSet ds(n);
+        for(int i=0;i<n; i++){
             for(int j=i+1;j<n;j++){
-                if(isConnected[i][j]==1){
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
+                if(isConnected[i][j] == 1){
+                    if(ds.find_par(i) == ds.find_par(j))continue;
+                    else{
+                        ds.union_by_size(i, j);
+                    }
                 }
+                
             }
         }
-        // now i have adjacency list
 
-        vector<int> vis(n,0);
         for(int i=0;i<n;i++){
-            if(vis[i]==false){
-                bfs(vis, adj, i);
-                res++;
-            }
+            if(ds.parent[i] == i)cnt++;
         }
-        return res;
+        return cnt;
     }
 };
