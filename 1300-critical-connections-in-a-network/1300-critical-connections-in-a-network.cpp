94fb351c68d3993b875de428312a74352183c784
mnishh997 <mnishh997@gmail.com>
2024-07-10 10:02:35 +0530

    Time: 389 ms (79.92%), Space: 167.8 MB (86.56%) - LeetHub

--- 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.cpp
+++ 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.cpp
@@ -1,37 +1,38 @@
 class Solution {
     int timer = 1;
-    void dfs(int node, int par, vector<bool>& vis, vector<int> adj[], int tin[], int low[], vector<vector<int>>& bridges){
-        vis[node] = 1;
+    void dfs(int node, int par, int tin[], int low[], vector<vector<int>>& res, vector<vector<int>>& adj, vector<bool>& vis){
+        vis[node] =true;
         tin[node] = low[node] = timer;
         timer++;
-        for(auto it: adj[node]){
-            if(it == par)continue;
+        for(int i=0;i<adj[node].size();i++){
+            if(adj[node][i] == par)continue;
             else{
-                if(vis[it] == true){
-                    low[node] = min(low[node], low[it]);
+                if(vis[adj[node][i]] == true){
+                    low[node] = min(low[node], low[adj[node][i]]);
                 }
                 else{
-                    dfs(it, node, vis, adj, tin, low, bridges);
-                    low[node] = min(low[node], low[it]);
-                    if(low[it]>tin[node]){
-                        bridges.push_back({it, node});
+                    dfs(adj[node][i], node, tin, low, res, adj, vis);
+                    low[node] = min(low[node], low[adj[node][i]]);
+                    if(low[adj[node][i]]>tin[node]){
+                        res.push_back({node, adj[node][i]});
                     }
                 }
             }
         }
-
     }
 public:
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
-        vector<int> adj[n];
+        // make the graph
+        vector<vector<int>> adj(n, vector<int>());
         for(int i=0;i<connections.size();i++){
             adj[connections[i][0]].push_back(connections[i][1]);
             adj[connections[i][1]].push_back(connections[i][0]);
         }
-        vector<bool> vis(n, 0);
+        // now I have the graph
         int tin[n], low[n];
-        vector<vector<int>> bridges;
-        dfs(0, -1, vis, adj, tin, low, bridges);
-        return bridges;
+        vector<vector<int>> res;
+        vector<bool> vis(n, false);
+        dfs(0, -1, tin, low, res, adj, vis);
+        return res;
     }
 };
