a9c4182323bf26bd0b602aee36f712dd42357c2f
mnishh997 <mnishh997@gmail.com>
2024-07-18 14:02:02 +0530

    Time: 23 ms (56.91%), Space: 25.9 MB (15.06%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
@@ -1,50 +1,45 @@
 class Solution {
 public:
-    string shortestCommonSupersequence(string str1, string str2) {
-        // first try to find the longest common subsequence
-        int n = str1.size(), m = str2.size();
+    string shortestCommonSupersequence(string s, string t) {
+        // first get the lcs
+        int n = s.size(), m = t.size();
         vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
-        
-
         for(int i=1;i<n+1;i++){
-            for(int j=1;j<=m;j++){
-                if(str1[i-1] == str2[j-1])dp[i][j] = dp[i-1][j-1] + 1;
-
+            for(int j=1;j<m+1;j++){
+                if(s[i-1] == t[j-1]){
+                    dp[i][j] = dp[i-1][j-1]+1;
+                }
                 else{
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
                 }
             }
         }
-        // dp[n][m] gives the lengeth of longest common subsequence
-        //let us now get the lcs 
         int i = n, j = m;
         string lcs = "";
-        while(dp[i][j] != 0){
-            if(str1[i-1] == str2[j-1]){
-                lcs+=str1[i-1];
-                j--;i--;
+        while(dp[i][j]!=0){
+            if(s[i-1] == t[j-1]){
+                lcs += s[i-1];
+                j--;
+                i--;
             }
             else{
-                if(dp[i-1][j] > dp[i][j-1])i--;
+                if(dp[i-1][j]>dp[i][j-1]){
+                    i--;
+                }
                 else j--;
             }
         }
 
         reverse(lcs.begin(), lcs.end());
-        //return lcs;
-        // ind will traverse on res string
-        if(lcs.size() == 0)return str1+str2;
-
-        int ind = 0;
-        int p = 0, q = 0;
-        string res ="";
+        int ind = 0;string res = "";
+        int q = 0, p = 0;
         while(ind<lcs.size()){
-            while(str1[p]!=lcs[ind]){
-                res+=str1[p];
+            while(s[p]!=lcs[ind]){
+                res+=s[p];
                 p++;
             }
-            while(str2[q] != lcs[ind]){
-                res+=str2[q];
+            while(t[q]!=lcs[ind]){
+                res+=t[q];
                 q++;
             }
             res+=lcs[ind];
@@ -52,12 +47,12 @@ public:
             p++;
             q++;
         }
-        while(p<str1.size()){
-            res+=str1[p];
-            p++;
+        while(p<s.size()){
+            res+=s[p];p++;
+
         }
-        while(q<str2.size()){
-            res+=str2[q];
+        while(q<t.size()){
+            res+=t[q];
             q++;
         }
         return res;
