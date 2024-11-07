0c56aebd5b9a5640ba56ecf4732aac5772d605b0
mnishh997 <mnishh997@gmail.com>
2024-11-07 21:38:19 +0530

    Time: 0 ms (100%), Space: 11.1 MB (70%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(), citations.end());
+        int size = citations.size();
+        int res = 0;
+        int i = 0;
+        while(size>=res) {
+            if(citations[i]>=res) {
+                res++;
+            }
+            else {
+                i++;
+                size--;
+            }
+        }
+        if(res == 0)return res;
+        return res-1;
+    }
+};
