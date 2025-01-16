7395fb2c36706eab3d05ee3ffdaca09dbf432589
mnishh997 <mnishh997@gmail.com>
2025-01-16 22:34:24 +0530

    Time: 0 ms (100%), Space: 64.2 MB (64.63%) - LeetHub

---
+++ 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size(), n2 = nums2.size();
+        int res = 0;
+        if(n1%2 == 1) {
+            for(auto it: nums2) {
+                res = res^it;
+            }
+        }
+        if(n2%2 == 1) {
+            for(auto it: nums1) {
+                res = res^it;
+            }
+        }
+        return res;
+    }
+};
